d23936215ae8c01d27b1ad1b1aa4a229b39f7a7f
fassabilf <fassabilf@gmail.com>
2026-02-09 14:37:40 +0000

    Fix SigLIP: Handle non-RGB images and BaseModelOutput cpu attribute error

--- mteb/models/model_implementations/siglip_models.py
+++ mteb/models/model_implementations/siglip_models.py
@@ -83,12 +83,15 @@ class SiglipModelWrapper(AbsEncoder):
 
         with torch.no_grad():
             for batch in tqdm(images):
+
+                batch_images = [img.convert("RGB") for img in batch["image"]]
+                
                 inputs = self.processor(
-                    images=batch["image"], return_tensors="pt", padding=True
+                    images=batch_images, return_tensors="pt", padding=True
                 )
                 inputs = {k: v.to(self.device) for k, v in inputs.items()}
                 image_outputs = self.model.get_image_features(**inputs)
-                all_image_embeddings.append(image_outputs.cpu())
+                all_image_embeddings.append(image_outputs.pooler_output.cpu())
         all_image_embeddings = torch.cat(all_image_embeddings, dim=0)
         return all_image_embeddings
 
