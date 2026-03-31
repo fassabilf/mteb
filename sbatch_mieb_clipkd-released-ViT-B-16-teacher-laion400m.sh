e0deea1bac0d9c9c43147d78dc03ae01ac05891d
Faiz Assabil Firdaus <fassabilf@gmail.com>
2026-03-31 20:43:01 +0700

    feat: add sbatch jobs for CLIP-KD released model MIEB(lite) eval

---
+++ sbatch_mieb_clipkd-released-ViT-B-16-teacher-laion400m.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -N 1 -c 16
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 25:00:00
+#SBATCH -A lt200394
+#SBATCH -J mieb_clipkd-released-ViT-B-16-teacher-laion400m
+#SBATCH -o /project/lt200394-thllmV/benchmark/mteb/logs/%x_%j.out
+
+export HF_HOME="/project/lt200394-thllmV/benchmark/.cache/huggingface"
+export HF_DATASETS_CACHE="/project/lt200394-thllmV/benchmark/.cache/huggingface/datasets"
+export HF_HUB_CACHE="/project/lt200394-thllmV/benchmark/.cache/huggingface/hub"
+export HF_DATASETS_OFFLINE=1
+export HF_HUB_OFFLINE=1
+
+module load Mamba/23.11.0-0
+conda activate mteb_env
+
+cd "/project/lt200394-thllmV/benchmark/mteb"
+python3 "/project/lt200394-thllmV/benchmark/mteb/eval_mieb.py" --model "fassabilf/clipkd-released-ViT-B-16-teacher-laion400m"
